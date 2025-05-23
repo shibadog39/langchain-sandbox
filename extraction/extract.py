161e53e66904b40ec074bf34c094f63a0dabd565
dvt-takahisa-sakurai <takahisa.sakurai@bcgdv.com>
2025-05-23 11:12:36 +0900

    Build an Extraction Chain
    https://python.langchain.com/docs/tutorials/extraction/

---
+++ extraction/extract.py
@@ -0,0 +1,98 @@
+import getpass
+import os
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+from typing import Optional, List
+from pydantic import BaseModel, Field
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+
+load_dotenv()
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+
+class Person(BaseModel):
+    """Information about a person."""
+
+    # ^ Doc-string for the entity Person.
+    # This doc-string is sent to the LLM as the description of the schema Person,
+    # and it can help to improve extraction results.
+
+    # Note that:
+    # 1. Each field is an `optional` -- this allows the model to decline to extract it!
+    # 2. Each field has a `description` -- this description is used by the LLM.
+    # Having a good description can help improve extraction results.
+    name: Optional[str] = Field(default=None, description="The name of the person")
+    hair_color: Optional[str] = Field(
+        default=None, description="The color of the person's hair if known"
+    )
+    height_in_meters: Optional[str] = Field(
+        default=None, description="Height measured in meters"
+    )
+
+class Data(BaseModel):
+    """Extracted data about people."""
+
+    # Creates a model so that we can extract multiple entities.
+    people: List[Person]
+
+# Define a custom prompt to provide instructions and any additional context.
+# 1) You can add examples into the prompt template to improve extraction quality
+# 2) Introduce additional parameters to take context into account (e.g., include metadata
+#    about the document from which the text was extracted.)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an expert extraction algorithm. "
+            "Only extract relevant information from the text. "
+            "If you do not know the value of an attribute asked to extract, "
+            "return null for the attribute's value.",
+        ),
+        # Please see the how-to about improving performance with
+        # reference examples.
+        # MessagesPlaceholder('examples'),
+        ("human", "{text}"),
+    ]
+)
+
+
+
+from langchain_core.utils.function_calling import tool_example_to_messages
+
+examples = [
+    (
+        "The ocean is vast and blue. It's more than 20,000 feet deep.",
+        Data(people=[]),
+    ),
+    (
+        "Fiona traveled far from France to Spain.",
+        Data(people=[Person(name="Fiona", height_in_meters=None, hair_color=None)]),
+    ),
+]
+
+
+messages = []
+
+for txt, tool_call in examples:
+    if tool_call.people:
+        # This final message is optional for some providers
+        ai_response = "Detected people."
+    else:
+        ai_response = "Detected no people."
+    messages.extend(tool_example_to_messages(txt, [tool_call], ai_response=ai_response))
+
+for message in messages:
+    message.pretty_print()
+
+
+message_no_extraction = {
+    "role": "user",
+    "content": "The solar system is large, but earth has only 1 moon.",
+}
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+structured_llm = llm.with_structured_output(schema=Data)
+structured_llm.invoke(messages + [message_no_extraction])
