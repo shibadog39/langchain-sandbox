fe346e206449833bbbb1cedca381120d432be6fc
dvt-takahisa-sakurai <takahisa.sakurai@bcgdv.com>
2025-05-23 10:27:30 +0900

    Classify Text into Labels
    https://python.langchain.com/docs/tutorials/classification/

---
+++ classification/classification.py
@@ -0,0 +1,47 @@
+import getpass
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai", temperature=0,)
+
+tagging_prompt = ChatPromptTemplate.from_template(
+    """
+Extract the desired information from the following passage.
+
+Only extract the properties mentioned in the 'Classification' function.
+
+Passage:
+{input}
+"""
+)
+
+
+class Classification(BaseModel):
+    sentiment: str = Field(..., enum=["happy", "neutral", "sad"])
+    aggressiveness: int = Field(
+        ...,
+        description="describes how aggressive the statement is, the higher the number the more aggressive",
+        enum=[1, 2, 3, 4, 5],
+    )
+    language: str = Field(
+        ..., enum=["spanish", "english", "french", "german", "italian"]
+    )
+
+
+# Structured LLM
+structured_llm = llm.with_structured_output(Classification)
+
+inp = "Estoy increiblemente contento de haberte conocido! Creo que seremos muy buenos amigos!"
+prompt = tagging_prompt.invoke({"input": inp})
+response = structured_llm.invoke(prompt)
+
+response
